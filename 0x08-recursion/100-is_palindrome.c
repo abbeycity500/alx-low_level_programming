4c66549dfe6c603827167644cea46eb45e0c2424
abbeycity500 <wahababiodun766@gmail.com>
2023-09-30 21:29:07 +0100

    They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,47 +1,43 @@
 #include "main.h"
 
-int check_pal(char *s, int i, int len);
-int _strlen_recursion(char *s);
-
+/**
+ * get_length - gets length of string
+ * @str: pointer to a string
+ * @len: length of a string
+ * Return: return length of string
+ */
+int get_length(char *str, int len)
+{
+	if (*(str + len) != '\0')
+	{
+		return (get_length(str, ++len));
+	}
+	return (--len);
+}
+/**
+ * check_pali - gets length of string
+ * @f: pointer to a string
+ * @r: length of a string
+ * @len: length of a string
+ * Return: return 0 if its not palindrome return 1 if it is
+ */
+int check_pali(char *f, char *r, int len)
+{
+	if (*f == *(r + len) && len > 1)
+	{
+		f++;
+		return (check_pali(f, r, --len));
+	}
+	if (len == 1)
+		return (1);
+	return (0);
+}
 /**
  * is_palindrome - checks if a string is a palindrome
- * @s: string to reverse
- *
- * Return: 1 if it is palindrome,0 if not
+ * @s: pointer to a string
+ * Return: return 0 if its not palindrome return 1 if it is
  */
 int is_palindrome(char *s)
 {
-	if (*s == 0)
-		return (1);
-	return (check_pal(s, 0, _strlen_recursion(s)));
-}
-
-/**
- * _strlen_recursion - returns the lenght of a string
- * @s: string to calculate lenght of
- *
- * Retutn: lenght of the string
- */
-int _strlen_recursion(char *s)
-{
-	if (*s == '\0')
-		return (0);
-	return (1 + _strlen_recursion(s + 1));
-}
-
-/**
- * check_pal - check the characters recursively for palindrome
- * @s: string to check
- * @i: iterator
- * @len: lenght of the string
- *
- * Return: 1 if palindrome, 0 if not
- */
-int check_pal(char *s, int i, int len)
-{
-	if (*(s + i) =! *(s + len - 1))
-		return (0);
-	if (i >= len)
-		return (1);
-	return (check_pal(s, i + 1, len - 1));
+	return (check_pali(s, s, get_length(s, 0)));
 }
