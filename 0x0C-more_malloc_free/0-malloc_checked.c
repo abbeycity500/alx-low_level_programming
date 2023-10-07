65e11fed19df3eb98f3174edc359ec5b658bd55b
abbeycity500 <wahababiodun766@gmail.com>
2023-10-07 14:18:59 +0100

    Trust no ome

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * malloc_checked - a function that allocates memory using malloc
+ * @b: size of memory
+ * if malloc fails, malloc_checked termination status value is 98
+ *
+ * Return: a ponter to the allocated memory
+ */
+void *malloc_checked(unsigned int b);
+{
+	int *ptr;
+
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return(ptr)
+}
