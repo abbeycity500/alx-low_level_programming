ce7b59e40d7b84d217dbe5c1b429c347e264b236
abbeycity500 <wahababiodun766@gmail.com>
2023-10-09 13:27:51 +0100

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include "main.h"
 
+
 /**
  * is_digit - checks if a string contains a non-digit char
  * @s: string to be evaluated
@@ -12,11 +13,10 @@ int is_digit(char *s)
 	int i = 0;
 
 
-
 	while (s[i])
 	{
 		if (s[i] < '0' || s[i] > '9')
-			returm (0);
+			return (0);
 		i++;
 	}
 	return (1);
@@ -31,7 +31,7 @@ int _strlen(char *s)
 {
 	int i = 0;
 
-	while ([i] != '\0')
+	while (s[i] != '\0')
 	{
 		i++;
 	}
@@ -66,7 +66,7 @@ int main(int argc, char *argv[])
 	len = len1 + len2 + 1;
 	result = malloc(sizeof(int) * len);
 	if (!result)
-		result (1);
+		return (1);
 	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
@@ -91,7 +91,7 @@ int main(int argc, char *argv[])
 			_putchar(result[i] + '0');
 	}
 	if (!a)
-		_putchar('0);
+		_putchar('0');
 	_putchar('\n');
 	free(result);
 	return (0);
