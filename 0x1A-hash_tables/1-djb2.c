4a0cabb32cc556fd3d266d966768bb3536fd89ad
abbeycity500 <wahababiodun766@gmail.com>
2023-12-23 00:46:05 +0100

    Hash files

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,19 +1,17 @@
-nclude "hash_tables.h"
+#include "hash_tables.h"
 
 /**
- * hash_djb2 - Hash function implementing the djb2 algorithm.
- * @str: The string to hash.
- * Created by Chukwudike Igwe
- * Return: The calculated hash.
- */
+  * hash_djb2 - Implements djb2 algorithm
+  * @str: key
+  * Return: hash
+  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-	unsigned long int hash;
+	unsigned long hash = 5381;
 	int c;
 
-	hash = 5381;/** hash 5381 */
 	while ((c = *str++))
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 
-	return (hash);/** returning hash */
+	return (hash);
 }
